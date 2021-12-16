c03a95b9b44245b5347d013a82bd3707ca49bb26
legendsjohn <john@letsbelegends.com>
2021-12-16 09:28:46 -0600

    add simple sign in

---
+++ src/Firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
+const auth = getAuth();
+connectAuthEmulator(auth, "http://localhost:9099");
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD9tNHU9n8yCQPaydXlu7WQro1SN6qVKfI",
+    authDomain: "legends-alpha.firebaseapp.com",
+    projectId: "legends-alpha",
+    storageBucket: "legends-alpha.appspot.com",
+    messagingSenderId: "187562843550",
+    appId: "1:187562843550:web:fdf935a6ac55836623ca8f",
+    measurementId: "${config.measurementId}",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(firebaseApp);
+
+connectDatabaseEmulator(database, "localhost", 9000);
+
+// Export Firebase singletons
+export { firebaseApp, database, auth };
