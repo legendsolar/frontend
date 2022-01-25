affa7bbc743cf20818ed3d098880435280377d1c
legendsjohn <john@letsbelegends.com>
2022-01-25 15:23:19 -0600

    add user update info

--- src/components/user_info.js
+++ src/components/user_info.js
@@ -1,42 +1,130 @@
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import UserDebugPaper from "./user_debug_paper";
-import UserAssetDebugPaper from "./user_asset_debug";
-import AssetLiveViewDebug from "./asset_live_view_debug";
-import MetricGauge from "./gauges/metric_gauge";
-import MetricSummary from "./summary/metric_summary";
 import useTheme from "@mui/material/styles/useTheme";
-import BasicProdWorm from "./worm/BasicProdWorm";
+import { ref, set } from "firebase/database";
+import { database } from "../Firebase";
 import { useAuth } from "../hooks/use_auth";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { TextField } from "@mui/material";
+import { useState } from "react";
 
 function UserInfo(props) {
     const auth = useAuth();
     const user = auth.user;
     const assetId = "-MtUpMiLZ0cvkQ-Dok2z";
     const theme = useTheme();
-    console.log(theme);
+
+    const saveUserInfo = () => {};
+
+    const startingValues = {
+        firstName: "",
+        lastName: "",
+        streetAddress: "",
+        city: "",
+        state: "",
+    };
+
+    const [formValues, setFormValues] = useState(startingValues);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        console.log(name, value);
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formValues);
+        set(ref(database, "users/" + user.uid + "/info"), formValues).then(
+            () => {
+                console.log("complete");
+            }
+        );
+    };
 
     return (
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-                <Grid item xs={8}>
-                    <Paper sx={{ minWidth: 275, p: 2 }}>
+                <Grid item xs={12}>
+                    <Paper
+                        sx={{
+                            minWidth: 275,
+                            p: 2,
+                            "& .MuiTextField-root": { m: 1 },
+                        }}
+                    >
                         <Typography
                             sx={{ fontSize: 14 }}
                             color="text.secondary"
                             gutterBottom
                         >
-                            User Info
+                            Your Contact Info
                         </Typography>
-                        <Typography variant="h6">Email</Typography>
+                        <Typography variant="h6">Email:</Typography>
                         <Typography variant="body2">{user.email}</Typography>
-                        <Typography variant="h6">GUI</Typography>
-                        <Typography variant="body2">{user.uid}</Typography>
-                        <Typography variant="h6">Address</Typography>
-                        <Typography variant="body2">{user.uid}</Typography>
+                        <Box
+                            sx={{
+                                "& .MuiTextField-root": { m: 1 },
+                            }}
+                        >
+                            <div>
+                                <TextField
+                                    name="firstName"
+                                    label="First Name"
+                                    variant="filled"
+                                    value={formValues.firstName}
+                                    onChange={handleInputChange}
+                                ></TextField>
+
+                                <TextField
+                                    name="lastName"
+                                    label="Last Name"
+                                    variant="filled"
+                                    value={formValues.lastName}
+                                    onChange={handleInputChange}
+                                ></TextField>
+
+                                <TextField
+                                    name="streetAddress"
+                                    label="Street Address"
+                                    variant="filled"
+                                    value={formValues.streetAddress}
+                                    onChange={handleInputChange}
+                                ></TextField>
+
+                                <TextField
+                                    name="aptNumber"
+                                    label="Apt #"
+                                    variant="filled"
+                                    value={formValues.aptNumber}
+                                    onChange={handleInputChange}
+                                ></TextField>
+
+                                <TextField
+                                    name="city"
+                                    label="City"
+                                    variant="filled"
+                                    value={formValues.city}
+                                    onChange={handleInputChange}
+                                ></TextField>
+
+                                <TextField
+                                    name="state"
+                                    label="State"
+                                    variant="filled"
+                                    value={formValues.state}
+                                    onChange={handleInputChange}
+                                ></TextField>
+                            </div>
+                        </Box>
+                        <Button variant="contained" onClick={handleSubmit}>
+                            Save
+                        </Button>
                     </Paper>
                 </Grid>
             </Grid>
