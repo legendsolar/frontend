fcb1748a0ad0298fc60f1a44879c23998df36972
legendsjohn <john@letsbelegends.com>
2022-02-01 12:44:41 -0600

    clean up cumulative gauge

--- src/components/gauges/cumulative_impact.js
+++ src/components/gauges/cumulative_impact.js
@@ -57,107 +57,137 @@ function CumulativeImpact(props) {
     }
 
     return (
-        <Paper sx={{ p: 2 }}>
-            <Typography variant="dashboardHeader" gutterBottom>
-                {displayOptions.title}
-            </Typography>
-
-            <Typography
-                sx={{
-                    fontWeight: 700,
-                    fontSize: 18,
-                    color: displayOptions.strokeColor,
-                }}
-                gutterBottom
+        <Paper sx={{ p: 2, width: 400, height: 288 }}>
+            <Stack
+                justifyContent="space-between"
+                spacing={1}
+                sx={{ height: "100%" }}
             >
-                {`${cumValue_unit.toFixed(2)}`}
-            </Typography>
+                <Typography variant="dashboardHeader" gutterBottom>
+                    {displayOptions.title}
+                </Typography>
 
-            <Typography
-                sx={{
-                    fontSize: 12,
-                    fontFamily: "Azeret Mono",
-                }}
-                color="text.primary"
-                gutterBottom
-            >
-                {displayOptions.unit}
-            </Typography>
-            <Typography
-                sx={{ fontSize: 12 }}
-                color="text.secondary"
-                gutterBottom
-            >
-                AVG
-            </Typography>
+                <Stack direction="row" justifyContent="space-evenly">
+                    <Stack>
+                        <Typography
+                            sx={{ fontSize: 12 }}
+                            color="text.secondary"
+                            align="center"
+                            gutterBottom
+                        >
+                            AVG
+                        </Typography>
 
-            <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
-                -
-            </Typography>
+                        <Typography
+                            sx={{ fontSize: 18 }}
+                            color="text.primary"
+                            align="center"
+                        >
+                            -
+                        </Typography>
+                    </Stack>
+                    <Stack>
+                        <Typography
+                            variant="unitMainDisplay"
+                            sx={{
+                                color: displayOptions.strokeColor,
+                            }}
+                            gutterBottom
+                            align="center"
+                        >
+                            {`${cumValue_unit.toFixed(2)}`}
+                        </Typography>
 
-            <Typography
-                sx={{ fontSize: 12 }}
-                color="text.secondary"
-                gutterBottom
-            >
-                BEST
-            </Typography>
+                        <Typography
+                            sx={{
+                                fontSize: 12,
+                                fontFamily: "Azeret Mono",
+                            }}
+                            color="text.primary"
+                            align="center"
+                        >
+                            {displayOptions.unit}
+                        </Typography>
+                    </Stack>
+                    <Stack>
+                        <Typography
+                            sx={{ fontSize: 12 }}
+                            color="text.secondary"
+                            gutterBottom
+                            align="center"
+                        >
+                            BEST
+                        </Typography>
 
-            <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
-                -
-            </Typography>
-            <Stack direction="row" spacing={1}>
-                <Chip
-                    label={"Y2D"}
-                    onClick={() => setHistoryState("year")}
-                    clickable={true}
-                    variant={historyState == "year" ? "filled" : "outlined"}
-                    sx={{
-                        textTransform: "uppercase",
-                        fontFamily: "Azeret Mono",
-                        fontSize: "9px",
-                        lineHeight: "10.5px",
-                    }}
-                ></Chip>
+                        <Typography
+                            sx={{ fontSize: 18 }}
+                            color="text.primary"
+                            align="center"
+                        >
+                            -
+                        </Typography>
+                    </Stack>
+                </Stack>
 
-                <Chip
-                    label={"Month to Date"}
-                    onClick={() => setHistoryState("month")}
-                    clickable={true}
-                    variant={historyState == "month" ? "filled" : "outlined"}
-                    sx={{
-                        textTransform: "uppercase",
-                        fontFamily: "Azeret Mono",
-                        fontSize: "9px",
-                        lineHeight: "10.5px",
-                    }}
-                ></Chip>
+                <Stack
+                    direction="row"
+                    spacing={1}
+                    justifyContent="space-evenly"
+                >
+                    <Chip
+                        label={"Y2D"}
+                        onClick={() => setHistoryState("year")}
+                        clickable={true}
+                        variant={historyState == "year" ? "filled" : "outlined"}
+                        sx={{
+                            textTransform: "uppercase",
+                            fontFamily: "Azeret Mono",
+                            fontSize: "9px",
+                            lineHeight: "10.5px",
+                        }}
+                    ></Chip>
 
-                <Chip
-                    label={"W2D"}
-                    onClick={() => setHistoryState("week")}
-                    clickable={true}
-                    variant={historyState == "week" ? "filled" : "outlined"}
-                    sx={{
-                        textTransform: "uppercase",
-                        fontFamily: "Azeret Mono",
-                        fontSize: "9px",
-                        lineHeight: "10.5px",
-                    }}
-                ></Chip>
+                    <Chip
+                        label={"Month to Date"}
+                        onClick={() => setHistoryState("month")}
+                        clickable={true}
+                        variant={
+                            historyState == "month" ? "filled" : "outlined"
+                        }
+                        sx={{
+                            textTransform: "uppercase",
+                            fontFamily: "Azeret Mono",
+                            fontSize: "9px",
+                            lineHeight: "10.5px",
+                        }}
+                    ></Chip>
 
-                <Chip
-                    label={"24H"}
-                    onClick={() => setHistoryState("day")}
-                    clickable={true}
-                    variant={historyState == "day" ? "filled" : "outlined"}
-                    sx={{
-                        textTransform: "uppercase",
-                        fontFamily: "Azeret Mono",
-                        fontSize: "9px",
-                        lineHeight: "10.5px",
-                    }}
-                ></Chip>
+                    <Chip
+                        label={"W2D"}
+                        onClick={() => setHistoryState("week")}
+                        clickable={true}
+                        variant={historyState == "week" ? "filled" : "outlined"}
+                        sx={{
+                            textTransform: "uppercase",
+                            fontFamily: "Azeret Mono",
+                            fontSize: "9px",
+                            lineHeight: "10.5px",
+                        }}
+                    ></Chip>
+
+                    <Chip
+                        label={"24H"}
+                        onClick={() => setHistoryState("day")}
+                        clickable={true}
+                        variant={historyState == "day" ? "filled" : "outlined"}
+                        sx={{
+                            textTransform: "uppercase",
+                            fontFamily: "Azeret Mono",
+                            fontSize: "9px",
+                            lineHeight: "10.5px",
+                        }}
+                    ></Chip>
+                </Stack>
             </Stack>
         </Paper>
     );
