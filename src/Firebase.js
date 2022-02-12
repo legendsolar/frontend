5c7cbdeeb900cb64925a828475ce34ed6c1d4590
legendsjohn <john@letsbelegends.com>
2022-02-11 21:58:44 -0600

    add multiple envs and commands

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,37 +1,42 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getApp, initializeApp } from "firebase/app";
 import { getDatabase, connectDatabaseEmulator } from "firebase/database";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
-import {
-    getAuth,
-    onAuthStateChanged,
-    connectAuthEmulator,
-} from "firebase/auth";
-import { useState, useEffect, useContext, createContext } from "react";
+const emulator =
+    !!process.env.REACT_APP_FIREBASE_EMULATOR &&
+    process.env.REACT_APP_FIREBASE_EMULATOR == "TRUE";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBMNSth898JP2cfGkoh3ZZd91AO-6RWTg4",
-    authDomain: "legends-alpha-prod.firebaseapp.com",
-    databaseURL: "https://legends-alpha-prod-default-rtdb.firebaseio.com",
-    projectId: "legends-alpha-prod",
-    storageBucket: "legends-alpha-prod.appspot.com",
-    messagingSenderId: "627679343299",
-    appId: "1:627679343299:web:2cc7d5603f538476256876",
-    measurementId: "G-8JZL5S5VET",
-};
+console.log("EMULATOR STATE: " + emulator);
+
+console.log({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
+
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
 
 const auth = getAuth(firebaseApp);
-
-// Get a reference to the database service
 const database = getDatabase(firebaseApp);
 
-const emulator = false;
-
 if (emulator) {
-    connectAuthEmulator(auth, "http://localhost:9099");
+    connectAuthEmulator(auth, "localhost", 9099);
     connectDatabaseEmulator(database, "localhost", 9000);
 }
-// Export Firebase singletons
+
 export { firebaseApp, database, auth };
