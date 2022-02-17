d818e9443745a6d9cd0c46232d70b0390505cda0
legendsjohn <john@letsbelegends.com>
2022-02-17 11:17:54 -0500

    update actions

--- .github/workflows/create_branch.sh
+++
@@ -1,9 +0,0 @@
-# git config --global user.name 'legendsolarbot'
-# git config --global user.email 'githubbotlegendssolar@gmail.com'
-# git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY
-# git checkout "${GITHUB_REF:11}"
-BRANCH_NAME=`date -u '+%d-%m-%Y_%H.%M.%S'` 
-git branch $BRANCH_NAME
-git checkout $BRANCH_NAME
-git commit -m "[BOT] Automated branch creation before deploy"
-git push --set-upstream origin $BRANCH_NAME
