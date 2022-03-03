520e64162e6e6a3b6df945224d070fb6e2c9104a
legendsjohn <john@letsbelegends.com>
2022-03-03 14:09:55 -0600

    wip commit

--- src/components/user_info.js
+++ src/components/user_info.js
@@ -191,6 +191,12 @@ function UserInfo(props) {
                     This information will be used to verify your ownership of
                     accounts provided and prevent fraud.{" "}
                 </Typography>
+
+                <Typography variant="description">
+                    {`[SANDBOX ONLY] To simulate the various statuses in the Sandbox, 
+                    submit either verified, retry, kba, document, or suspended 
+                    in the firstName parameter `}
+                </Typography>
             </Stack>
 
             <Grid container spacing={2} sx={{ width: "100%", mt: 1 }}>
