b9e350e32ff573e3aea103a09e323ba9597ab123
legendsjohn <john@letsbelegends.com>
2022-03-17 07:38:01 -0600

    add disclaimer

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,13 +15,13 @@ const Footer = (props) => {
                                             Company
                                         </Typography>
                                         <Typography variant="body2">
-                                            Home
+                                            Reserve Panels
                                         </Typography>
                                         <Typography variant="body2">
-                                            Available panels
+                                            About us
                                         </Typography>
                                         <Typography variant="body2">
-                                            About Us
+                                            The team
                                         </Typography>
                                     </Stack>
                                 </Grid>
@@ -29,16 +29,16 @@ const Footer = (props) => {
                                 <Grid item xs={12} md={4}>
                                     <Stack>
                                         <Typography variant="smallHeadline">
-                                            Company
+                                            Learn
                                         </Typography>
                                         <Typography variant="body2">
-                                            Home
+                                            How it works
                                         </Typography>
                                         <Typography variant="body2">
-                                            Available panels
+                                            FAQs
                                         </Typography>
                                         <Typography variant="body2">
-                                            About Us
+                                            News Room
                                         </Typography>
                                     </Stack>
                                 </Grid>
@@ -46,16 +46,16 @@ const Footer = (props) => {
                                 <Grid item xs={12} md={4}>
                                     <Stack>
                                         <Typography variant="smallHeadline">
-                                            Company
+                                            Connect
                                         </Typography>
                                         <Typography variant="body2">
-                                            Home
+                                            Twitter
                                         </Typography>
                                         <Typography variant="body2">
-                                            Available panels
+                                            Instagram
                                         </Typography>
                                         <Typography variant="body2">
-                                            About Us
+                                            LinkedIn
                                         </Typography>
                                     </Stack>
                                 </Grid>
@@ -108,7 +108,7 @@ const Footer = (props) => {
                                             Nera Lerner
                                         </Typography>
                                         <Typography variant="label">
-                                            Cheif Operating Officer
+                                            Co-Founder & COO
                                         </Typography>
                                     </Stack>
                                 </Stack>
@@ -124,7 +124,7 @@ const Footer = (props) => {
                                             Lassor Feasley
                                         </Typography>
                                         <Typography variant="label">
-                                            Cheif Executive Officer
+                                            Co-Founder & CEO
                                         </Typography>
                                     </Stack>
                                 </Stack>
@@ -144,7 +144,98 @@ const Footer = (props) => {
                 <div></div>
             </Grid>
 
-            <div style={{ height: "300px" }}></div>
+            <Stack sx={{ mt: 8 }}>
+                <Typography variant="subtitle3">Disclaimer</Typography>
+                <div>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <Typography variant="description">
+                                    No offers to sell or solicitations of an
+                                    offer to buy securities or any other type of
+                                    investment are being made or solicited by
+                                    Legends Incorporated at this time. No money
+                                    or other consideration is being solicited,
+                                    and if sent in response, will not be
+                                    accepted. Any indication of interest that
+                                    you may provide (including by submitting an
+                                    email address at which you can be contacted
+                                    or receive further information) involves no
+                                    obligation or commitment of any kind.
+                                </Typography>
+
+                                <Typography variant="description">
+                                    Legends Incorporated is not a registered
+                                    broker-dealer, investment adviser, or
+                                    crowdfunding portal and does not engage in
+                                    any activities requiring any such
+                                    registration. The information contained on
+                                    this website is provided for informational
+                                    and discussion purposes only and is not
+                                    intended to be a recommendation to invest
+                                    in, or purchase, sell, or hold, any
+                                    securities, or to engage in any type of
+                                    transaction. Any such offers will only be
+                                    made pursuant to formal offering materials
+                                    containing full details regarding risks,
+                                    investment terms, and fees.
+                                </Typography>
+
+                                <Typography variant="description">
+                                    Please consult with your legal and financial
+                                    advisors before investing and do not invest
+                                    unless you are able to sustain the risk of
+                                    loss of your entire investment. Past
+                                    performance is no guarantee of future
+                                    results. Any historical returns, expected
+                                    returns, or probability projections are
+                                    forward looking statements and may not
+                                    reflect actual future performance. All
+                                    investments involve risk and may result in
+                                    loss.
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Stack>
+                                <Typography variant="description">
+                                    This website contains forward-looking
+                                    statements that are based on our beliefs and
+                                    assumptions and on information currently
+                                    available to us. In some cases, you can
+                                    identify forward-looking statements by the
+                                    following words: "estimated," “will,”
+                                    “expect,” “would,” “intend,” “believe,” or
+                                    other comparable terminology.
+                                </Typography>
+
+                                <Typography variant="description">
+                                    Forward-looking statements in this website
+                                    include, but are not limited to, statements
+                                    about our business plan, potential
+                                    investments in renewable energy projects,
+                                    our market opportunities and beliefs, and
+                                    our objectives for future operations. These
+                                    statements involve risks, uncertainties,
+                                    assumptions and other factors that may cause
+                                    actual results or performance to be
+                                    materially different. We cannot assure you
+                                    that the forward-looking statements will
+                                    prove to be accurate. These forward-looking
+                                    statements speak only as of the date hereof.
+                                    We disclaim any obligation to update these
+                                    forward-looking statements.
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                    </Grid>
+                </div>
+                <Typography variant="subtitle3">
+                    © 2022 Legends Incorporated. All rights reserved.
+                </Typography>
+            </Stack>
+
+            <div style={{ height: "100px" }}></div>
         </Box>
     );
 };
