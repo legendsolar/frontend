1ae9127232e21f78d30286b8f69c2a147304a40b
legendsjohn <john@letsbelegends.com>
2022-03-22 12:09:00 -0700

    add a little data to asset object

--- src/utils/asset_data.js
+++ src/utils/asset_data.js
@@ -11,6 +11,60 @@ const investmentOpportunities = {
                 value: "$74,500",
             },
         ],
+        content: [
+            {
+                headline: `
+                Legends Incorporated is offering equity in four separate
+                commercial solar facilities to accredited investors.`,
+                paragraphs: [
+                    `Two facilities are in in New Jersey and two in
+                        California. Each solar facility is on an ‘Extra Space
+                        Mini Storage’ managed property owned by the REIT Merit
+                        Hill Capital.`,
+                    `Once operational, these facilities will earn a stream of
+                        payments from Extra Space as well as tax benefits.
+                        Payments will be fixed, regardless of solar
+                        productivity.
+                    `,
+
+                    `To effectively monetize the tax benefits this investment
+                        provides, you should meet the following criteria:`,
+
+                    `Up to ~55% of net return will be attributable to tax
+                        incentives. The remaining return is derived from power
+                        purchase payments from Merit Hill. These power purchase
+                        payments are shielded from federal income taxes via
+                        depreciation benefits. Please carefully review the
+                        attached pro forma and discuss if it is right for you
+                        with your financial advisors. As always, feel free to
+                        reach out to me with additional questions.`,
+                ],
+            },
+            {
+                headline: `
+                About Merit Hill and Extra Space `,
+                paragraphs: [
+                    `Merit Hill is a leading self-storage owner/operator with
+                    more than 200 properties in 33 states. Extra Space Mini
+                    Storage manages and operates dozens of Merit Hill
+                    properties. Once operational, these facilities will earn
+                    a stream of payments from Extra Space as well as tax
+                    benefits. Payments will be fixed, regardless of solar
+                    productivity.`,
+
+                    `Once operational, these facilities will earn a stream of
+                        payments from Extra Space as well as tax benefits.
+                        Payments will be fixed, regardless of solar
+                        productivity.
+                    `,
+
+                    `Merit Hill will both be the only source of revenue and
+                    the purchaser of this facility after the 7 year holding
+                    period.`,
+                ],
+            },
+        ],
+
         color: "skyBlue",
     },
 
