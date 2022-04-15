5280af6d8f6ea6bdbfb9af9a75592d5059d63925
legendsjohn <john@letsbelegends.com>
2022-04-15 12:52:21 -0500

    add analysis

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     rules: {
         'react/react-in-jsx-scope': 'off',
         'no-irregular-whitespace': 'off',
+        'absolute-imports/only-absolute-imports': ['error'],
     },
 };
