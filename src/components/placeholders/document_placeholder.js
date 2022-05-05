e8a8784fab76b01ad3728da8b4f793f367d2dfa0
legendsjohn <john@letsbelegends.com>
2022-05-05 11:33:54 -0500

    add document placeholder

--- src/components/placeholders/document_placeholder.js
+++ src/components/placeholders/document_placeholder.js
@@ -1,17 +1,47 @@
-import {Typography} from '@mui/material';
+import {Typography, Grid} from '@mui/material';
+import DocumentComponent from 'components/invest/document_component';
 import DefaultComponent from 'components/utils/default_component';
 
 const DocumentPlaceholder = () => {
     return (
         <div>
-            <DefaultComponent>
-                <Typography variant="smallHeadline">
-                    You have not purchased any panels yet
-                </Typography>
-                <Typography variant="headline1">
-                    Click to view available panels
-                </Typography>
-            </DefaultComponent>
+            <Grid container spacing={4} sx={{width: '100%'}}>
+                <Grid item xs={12}>
+                    <DefaultComponent>
+                        <Typography variant="smallHeadline">
+                            Purchase panels to view documents
+                        </Typography>
+                        <Typography variant="headline1">
+                            You don't have any doucments.
+                        </Typography>
+                    </DefaultComponent>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <DefaultComponent disabled={true}>
+                        <Typography variant="smallHeadline">
+                            Solar Investments
+                        </Typography>
+                        <DocumentComponent
+                            documents={[
+                                {
+                                    title: 'Test Document 1',
+                                    color: 'legendaryGreen',
+                                },
+                                {
+                                    title: 'Test Document 2',
+                                    color: 'skyBlue',
+                                },
+                                {
+                                    title: 'Test Document 3',
+                                    color: 'pencilYellow',
+                                },
+                            ]}
+                            onDownloadAttempt={() => {}}
+                        ></DocumentComponent>
+                    </DefaultComponent>
+                </Grid>
+            </Grid>
         </div>
     );
 };
