2c16e97cb59a5c695aedfe604177fc11bb1dda06
John Compas <johncompas@Johns-MacBook-Air.local>
2022-09-07 12:54:28 -0700

    properly format single time units

--- src/utils/date_formatter.ts
+++ src/utils/date_formatter.ts
@@ -10,13 +10,13 @@ export const dateDifferenceHumanReadable = (
 ): string => {
     const minutes = differenceInMinutes(left, right);
 
-    if (minutes < 60) return `${minutes} minutes`;
+    if (minutes < 60) return `${minutes} minute` + (minutes === 1 ? '' : 's');
 
     const hours = differenceInHours(left, right);
 
-    if (hours < 25) return `${hours} hours`;
+    if (hours < 25) return `${hours} hour` + (hours === 1 ? '' : 's');
 
     const days = differenceInDays(left, right);
 
-    return `${days} days`;
+    return `${days} day` + (days === 1 ? '' : 's');
 };
