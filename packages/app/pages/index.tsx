be60855acc45a540572261c6c1eadc36495a393f
John Compas <johncompas@Johns-MacBook-Air.local>
2022-11-08 16:42:26 -0800

    next kinda working

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,20 +1,9 @@
-import { Worm, WormProps, defaultChartDisplayParams} from '@project/components';
-import { Component, generateFakeProductionData} from '@project/components';
+import React, {useState} from 'react';
+import {LivePill, ReactFromModule} from '@project/components/pills';
 
 export default function Index() {
+    console.log(React === ReactFromModule);
 
-    
-
-  return (
-    <div>
-        <Worm
-            rawData={generateFakeProductionData(7,3000)} 
-            loading={false}
-            error={false}
-            styleOptions={defaultChartDisplayParams}
-            max_W={3000}
-            sx= {{}}
-        ></Worm>
-    </div>
-  );
+    const [state, setState] = useState('test');
+    return <div>{state}</div>;
 }
--- packages/components/pills/live_pill.tsx
+++ packages/components/pills/live_pill.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Chip } from '@mui/material';
+import {Chip} from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
 
-export const LivePill = ({ error }: { error: string }) => {
-  return (
-    <Chip
-      size="small"
-      variant={'light' as any}
-      avatar={
-        <CircleIcon
-          fontSize="small"
-          color="action"
-          style={{
-            color: error ? 'red' : 'green',
-            width: '10px',
-            height: '10px',
-          }}
-        />
-      }
-      label={error ? 'error' : 'live'}
-      sx={{
-        fontFamily: 'Be Vietnam Pro',
-        textTransform: 'uppercase',
-        fontSize: '18px',
-        fontWeight: 300,
-        color: 'blackDawn.main',
-      }}
-    ></Chip>
-  );
-};
+export {default as ReactFromModule} from 'react';
 
+export const LivePill = ({error}: {error: string}) => {
+    return (
+        <Chip
+            size="small"
+            variant={'light' as any}
+            avatar={
+                <CircleIcon
+                    fontSize="small"
+                    color="action"
+                    style={{
+                        color: error ? 'red' : 'green',
+                        width: '10px',
+                        height: '10px',
+                    }}
+                />
+            }
+            label={error ? 'error' : 'live'}
+            sx={{
+                fontFamily: 'Be Vietnam Pro',
+                textTransform: 'uppercase',
+                fontSize: '18px',
+                fontWeight: 300,
+                color: 'blackDawn.main',
+            }}
+        ></Chip>
+    );
+};
