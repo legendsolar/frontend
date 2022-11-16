6ca5d37f879576e349bfea3e752c78e1f0989dcf
John Compas <johncompas@Johns-MacBook-Air.local>
2022-11-16 10:59:56 -0800

    fix chart axis

--- packages/components/map/map_terrain_3d.tsx
+++ packages/components/map/map_terrain_3d.tsx
@@ -1,60 +1,60 @@
-import useInterval from '@project/hooks/use_interval';
-import {useRef, useState, useEffect, useCallback} from 'react';
-import Map from 'react-map-gl';
+import { useInterval } from "@project/hooks/use_interval";
+import { useRef, useState, useEffect, useCallback } from "react";
+import Map from "react-map-gl";
 
 interface MapTerrain3DProps {
-    width: string;
-    height: string;
-    lat: number;
-    lng: number;
-    zoom: number;
-    markers?: Array<JSX.Element>;
-    initBearing: number;
+  width: string;
+  height: string;
+  lat: number;
+  lng: number;
+  zoom: number;
+  markers?: Array<JSX.Element>;
+  initBearing: number;
 }
 
 export const MapTerrain3D = ({
-    width,
-    height,
-    lat,
-    lng,
-    zoom,
-    markers,
-    initBearing,
+  width,
+  height,
+  lat,
+  lng,
+  zoom,
+  markers,
+  initBearing,
 }: MapTerrain3DProps) => {
-    const mapRef: any = useRef();
+  const mapRef: any = useRef();
 
-    const time = 1000;
+  const time = 1000;
 
-    const [bearing, setBearing] = useState(initBearing);
+  const [bearing, setBearing] = useState(initBearing);
 
-    useInterval(() => setBearing((bearing + 1) % 360), time);
+  useInterval(() => setBearing((bearing + 1) % 360), time);
 
-    useEffect(() => {
-        if (mapRef.current) {
-            mapRef.current.easeTo({bearing, duration: time, easing: (x) => x});
-        }
-    }, [bearing]);
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.easeTo({ bearing, duration: time, easing: (x) => x });
+    }
+  }, [bearing]);
 
-    return (
-        <Map
-            initialViewState={{
-                longitude: lng,
-                latitude: lat,
-                zoom: zoom,
-                bearing: 0,
-                pitch: 80,
-            }}
-            maxPitch={85}
-            ref={mapRef}
-            style={{width, height}}
-            mapStyle={'mapbox://styles/lassorfeasley/cl83yjanv002z15mvq9wu36io'}
-            dragPan={false}
-            dragRotate={false}
-            touchZoomRotate={false}
-            scrollZoom={false}
-            terrain={{source: 'mapbox-dem', exaggeration: 1.0}}
-        >
-            {markers?.map((marker) => marker)}
-        </Map>
-    );
+  return (
+    <Map
+      initialViewState={{
+        longitude: lng,
+        latitude: lat,
+        zoom: zoom,
+        bearing: 0,
+        pitch: 80,
+      }}
+      maxPitch={85}
+      ref={mapRef}
+      style={{ width, height }}
+      mapStyle={"mapbox://styles/lassorfeasley/cl83yjanv002z15mvq9wu36io"}
+      dragPan={false}
+      dragRotate={false}
+      touchZoomRotate={false}
+      scrollZoom={false}
+      terrain={{ source: "mapbox-dem", exaggeration: 1.0 }}
+    >
+      {markers?.map((marker) => marker)}
+    </Map>
+  );
 };
