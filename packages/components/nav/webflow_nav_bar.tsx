aad24a95fc4be9397178572570b8c0da744c52e1
John Compas <john@letsbelegends.com>
2023-01-20 12:53:46 -0800

    fix new logo going bonkers

--- packages/components/nav/webflow_nav_bar.tsx
+++ packages/components/nav/webflow_nav_bar.tsx
@@ -105,7 +105,10 @@ export const WebflowNavBar = ({
           <Image
             src={TypemarkSolarSVG}
             style={{
-              width: "125px",
+              maxWidth: "125px",
+              width: "auto",
+              height: "auto",
+              objectFit: "contain",
             }}
           ></Image>
 
